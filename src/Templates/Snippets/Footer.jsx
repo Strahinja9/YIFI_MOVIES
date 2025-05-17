69dccbc91372343872597ece8ebacd6ecb38591a
Strahinja <151401213+Strahinja9@users.noreply.github.com>
2025-05-18 00:26:01 +0200

    * Initial commit

--- src/Templates/Snippets/Footer.jsx
+++ src/Templates/Snippets/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
 
         <div className="bg-dark bg-gradient">
-            <p style={{color:"white", textAlign:"center"}} className="p-3 m-0"> &copy;2025 YIFI Movies</p>
+            <p style={{color:"white", textAlign:"center"}} className="p-3 m-0"> &copy;2025 YIFI Movies!</p>
         </div>
     )
 }
